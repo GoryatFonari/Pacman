34996c0a1042de872bd2d9390498cacaae17e3bf
GoryatFonari <perelygin.ad@edu.spbstu.ru>
2022-06-01 12:40:07 +0300

    Initial commit

---
+++ core/src/com/core/game/PacmanGame.java
@@ -0,0 +1,13 @@
+package com.core.game;
+
+import com.badlogic.gdx.Game;
+
+public class PacmanGame extends Game {
+    PacmanLevel tl;
+
+    public void create()
+    {
+        tl = new PacmanLevel(this);
+        setScreen( tl);
+    }
+}
---
+++ desktop/src/com/core/game/DesktopLauncher.java
@@ -0,0 +1,17 @@
+package com.core.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.core.game.PacmanLevel;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle("PacmanDemo");
+		PacmanGame myGame = new PacmanGame();
+		Lwjgl3Application launcher = new Lwjgl3Application(myGame, config);
+	}
+}
